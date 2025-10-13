9cda40237c98c198c401b47e6c7ad3738a805177
Izaac-Cardoso <izaacrcardoso@gmail.com>
2025-10-13 20:11:52 -0300

    Refactored dto by adding new parameters

--- src/main/java/com/izaac_cardoso/qrcode/generator/dtos/DTOResponse.java
+++ src/main/java/com/izaac_cardoso/qrcode/generator/dtos/DTOResponse.java
@@ -1,5 +1,16 @@
 package com.izaac_cardoso.qrcode.generator.dtos;
 
-public record DTOResponse(
-        String url
+public record DTOResponse (
+//insert field validation
+        String id,
+        String date,
+        double pH,
+        double tds,
+        double color,
+        double turbidity,
+        double nitrite,
+        double nitrate,
+        double phosphorus,
+        double chlorine,
+        boolean ecoli
 ) { }
