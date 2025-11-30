c71db57f9befeda8dd2794f451ab24ed67b513dd
Izaac-Cardoso <izaacrcardoso@gmail.com>
2025-11-30 10:24:55 -0300

    A query method has been added to the Repository

--- src/main/java/com/izaac_cardoso/qrcode/generator/domain/service/SampleService.java
+++ src/main/java/com/izaac_cardoso/qrcode/generator/domain/service/SampleService.java
@@ -4,9 +4,7 @@ import com.izaac_cardoso.qrcode.generator.domain.entities.Sample;
 import com.izaac_cardoso.qrcode.generator.domain.exceptions.NotFoundException;
 import com.izaac_cardoso.qrcode.generator.repository.CollectedSampleRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -26,14 +24,11 @@ public class SampleService {
     }
 
     public ResponseEntity<Sample> findSample(String sampleId, LocalDateTime date) {
-        Sample sample;
-        try {
-            sample = repository.findByIdAndDate(sampleId, date);
+        var sample = repository.findByIdAndDate(sampleId, date)
+                     .orElseThrow(() -> new NotFoundException(
+                             String.format("Sample with id: %s could not be found.",sampleId)
+                     ));
 
-            return ResponseEntity.ok(sample);
-
-        } catch (NotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return ResponseEntity.ok(sample);
     }
 }
--- src/main/java/com/izaac_cardoso/qrcode/generator/repository/CollectedSampleRepository.java
+++ src/main/java/com/izaac_cardoso/qrcode/generator/repository/CollectedSampleRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 
 @Repository
 public interface CollectedSampleRepository extends JpaRepository<Sample, String> {
 
-    Sample findByIdAndDate(String id, LocalDateTime date);
+    public Optional<Sample> findByIdAndDate(String id, LocalDateTime date);
 }
